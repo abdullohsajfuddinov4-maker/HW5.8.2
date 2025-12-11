ccbfd8e18d973a4586e9aa7cdb3228c5c0b4457c
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-12-11 12:11:56 +0500

    full beccend

--- produkt/urls.py
+++ produkt/urls.py
@@ -4,5 +4,8 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('product/<int:pk>',views.product,name='product'),
     path('full/<int:pk>',views.desc,name="full"),
-    path('cretae_product/',views.create_product,name='create')
-    ]
+    path('create_product/', views.create_product, name='create'),
+    path('updata_product/<int:pk>/', views.updata_product, name='updata'),
+    path('del_product/<int:pk>/', views.del_product, name='del'),
+
+]
--- produkt/views.py
+++ produkt/views.py
@@ -18,31 +18,62 @@ def desc(request,pk):
     context = {'products':products}
     return render(request,'desc.html',context)
 
+from django.shortcuts import render, redirect
+from .models import Product, Make
+
 def create_product(request):
     if request.method == 'POST':
-        name_product = request.POST.get('name_product','')
-        make = request.POST.get('make','')
-        price = request.POST.get('price','')
-        county = request.POST.get('county','')
-        create_at = request.POST.get('create_at','')
-        desc = request.POST.get('desc','')
-        image = request.FILES['image']
-
+        name_product = request.POST.get('name_product', '')
+        make = Make.objects.filter(name=request.POST.get('make', '')).first()
+        price = request.POST.get('price', '')
+        county = request.POST.get('county', '')
+        desc = request.POST.get('desc', '')
+        image = request.FILES.get('image')
 
         product = Product(
             name_product=name_product,
             make=make,
             price=price,
             county=county,
-            create_at=create_at,
             desc=desc,
             image=image,
         )
-
+        messages.success(request,'product qoshildi')
         product.save()
 
+        return redirect('full', pk=product.pk)
 
-        redirect('full',product.pk)
+    return render(request, 'create_product.html')
 
-    return render(request,'create_product.html')
+
+
+def updata_product(request, pk):
+    product = Product.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        product.name_product = request.POST.get('name_product', '')
+        make_name = request.POST.get('make', '')
+
+        make_instance = Make.objects.filter(name=make_name).first()
+        if make_instance:
+            product.make = make_instance
+        product.price = request.POST.get('price', 0)
+        product.county = request.POST.get('county', '')
+        product.desc = request.POST.get('desc', '')
+        if 'image' in request.FILES:
+            product.image = request.FILES['image']
+
+        product.save()
+        return redirect('full', pk=product.pk)
+
+
+    return render(request, 'updata.html', {'product': product})
+
+
+def del_product(request,pk):
+    product = Product.objects.filter(pk=pk).first()
+    if request.method == 'POST':
+        product.delete()
+        return redirect('home')
+    return render(request,'del_product.html',{'product':product})
 
